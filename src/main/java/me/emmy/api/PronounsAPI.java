45ba4a52cb19d2f67506fee74aca8f6fb701dea6
Emmy <158222177+hmEmmy@users.noreply.github.com>
2025-04-05 17:58:19 +0200

    feat: making things fancy

--- src/main/java/me/emmy/api/PronounsAPI.java
+++ src/main/java/me/emmy/api/PronounsAPI.java
@@ -2,20 +2,50 @@ package me.emmy.api;
 
 import lombok.Getter;
 import me.emmy.api.repository.PlayerRepository;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
 
 @Getter
 public class PronounsAPI {
     private final JavaPlugin plugin;
     private final PlayerRepository playerRepository;
 
+    private final String version = "1.0.0";
+    private final String author = "Emmy";
+    private final String description = "A simple API for managing player pronouns in Minecraft plugins.";
+
+    private final boolean usingDefaultRepository;
+
     /**
      * Constructor for the PronounsAPI class.
      *
      * @param plugin The JavaPlugin instance.
      */
-    public PronounsAPI(JavaPlugin plugin) {
+    public PronounsAPI(JavaPlugin plugin, boolean initializeBasicRepository) {
         this.plugin = plugin;
-        this.playerRepository = new PlayerRepository();
+
+        if (initializeBasicRepository) {
+            this.playerRepository = new PlayerRepository();
+            this.usingDefaultRepository = true;
+        } else {
+            this.playerRepository = null;
+            this.usingDefaultRepository = false;
+        }
+
+        Arrays.asList(
+                "",
+                "&d&lPronounsAPI successfully initiated!",
+                " &f" + this.description,
+                "",
+                " &fAuthor: &d" + this.author,
+                " &fVersion: &d" + this.version,
+                "",
+                this.usingDefaultRepository ? " &dUsing default repository!" : " &aUsing custom repository!",
+                " &fHooked into: &d" + plugin.getName(),
+                ""
+        ).forEach(message -> Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', message)));
     }
 }
