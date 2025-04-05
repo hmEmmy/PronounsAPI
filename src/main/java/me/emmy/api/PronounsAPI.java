2b01ab3ad1cec9412bfdbaa53427b90a3c07a3de
Emmy <158222177+hmEmmy@users.noreply.github.com>
2025-04-05 17:04:05 +0200

    Initial commit

---
+++ src/main/java/me/emmy/api/PronounsAPI.java
@@ -0,0 +1,17 @@
+package me.emmy.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PronounsAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
